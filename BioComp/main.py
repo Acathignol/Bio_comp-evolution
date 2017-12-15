a18e92fe39e8682c3e46666d7acc1e55b6b848f8
Annie <annie@wifinsa422.insa-lyon.fr>
2017-12-15 11:13:39 +0100

    main.py need Antoine because there is a problem of variables

--- BioComp/main.py
+++ BioComp/main.py
@@ -19,6 +19,9 @@ goal_profile = [25,250,400,22,300,44,500,230,145,957] # sys.argv[2]
 global percent_zyup
 percent_zyup = 1 # sys.argv[3] # 1 = 100%
 
+global Genome_fitness
+Genome_fitness = 0
+# TestMain.calc_fitness(Transcrit) #problem : il est en global dans l'autre, antoine ? help !
 TestMain.Metropolis(nb_iterations)
 
 print Genome_pos
