17e78f0f19d6b0dbf713f5a56f590e9d5288e871
WaitndSee <antoine.weber@insa-lyon.fr>
2017-12-15 11:19:18 +0100

    syntax fix

--- BioComp/TestMain.py
+++ BioComp/TestMain.py
@@ -52,75 +52,73 @@ def load_genome(directoryAddress):
 	
 	  
 def changeGenomeDir(Dir_curr):
-	reorder()
-	writeProt_dat(Dir_curr)
-	write_gff(Dir_curr)
-	writeTTS_dat(Dir_curr)
-	writeTSS_dat(Dir_curr)
+  reorder()
+  writeProt_dat(Dir_curr)
+  write_gff(Dir_curr)
+  writeTTS_dat(Dir_curr)
+  writeTSS_dat(Dir_curr)
 
 
 def reorder():
-	dostuff=0
+  dostuff=0
 
 def writeProt_dat(directoryAddress):
-	global Prot_pos
-	newtab = []
-	newtab.append("prot_name\tprot_pos\n")
-	for i in range(len(Genome_pos)):
-		newtab.append("hns"+"\t"+str(Prot_pos[i])+"\n")
-	f = open(directoryAddress+'prot.dat','w')
-	f.writelines(newtab)
-	f.close()
+  global Prot_pos
+  newtab = []
+  newtab.append("prot_name\tprot_pos\n")
+  for i in range(len(Genome_pos)):
+  	newtab.append("hns"+"\t"+str(Prot_pos[i])+"\n")
+  f = open(directoryAddress+'prot.dat','w')
+  f.writelines(newtab)
+  f.close()
 
 def write_gff(directoryAddress):
-	global Range,Genome_pos,Genome_sign,Genome_names
-	f = open(directoryAddress+"tousgenesidentiques.gff","r")
-	lignes = f.readlines()
-	f.close()
-	newtab = lignes[0:3]
-	newtab.append(lignes[3].split(" ")[0]+" "+lignes[3].split(" ")[1]+" "+Range[0]+" "+Range[1]+"\n")
-	newtab.append(lignes[4].split("\t")[0]+"\t"+lignes[4].split("\t")[1]+"\t"+lignes[4].split("\t")[2]+"\t"+Range[0]+"\t"+Range[1]+"\t.\t+\t.\tID=id0;Name=tousgenesidentiques\n")
-	for i in range(len(Genome_pos)):
-		s = "tousgenesidentiques\tRefSeq\tgene\t"
-		newtab.append(s+str(Genome_pos[i][0])+"\t"+str(Genome_pos[i][1])+"\t.\t"+Genome_sign[i]+"\t.\tID=g1;Name="+Genome_names[i])
-	f = open(directoryAddress+"tousgenesidentiques.gff","w")
-	f.writelines(newtab)
-	f.close()
+  global Range,Genome_pos,Genome_sign,Genome_names
+  f = open(directoryAddress+"tousgenesidentiques.gff","r")
+  lignes = f.readlines()
+  f.close()
+  newtab = lignes[0:3]
+  newtab.append(lignes[3].split(" ")[0]+" "+lignes[3].split(" ")[1]+" "+Range[0]+" "+Range[1]+"\n")
+  newtab.append(lignes[4].split("\t")[0]+"\t"+lignes[4].split("\t")[1]+"\t"+lignes[4].split("\t")[2]+"\t"+Range[0]+"\t"+Range[1]+"\t.\t+\t.\tID=id0;Name=tousgenesidentiques\n")
+  for i in range(len(Genome_pos)):
+    s = "tousgenesidentiques\tRefSeq\tgene\t"
+  newtab.append(s+str(Genome_pos[i][0])+"\t"+str(Genome_pos[i][1])+"\t.\t"+Genome_sign[i]+"\t.\tID=g1;Name="+Genome_names[i])
+  f = open(directoryAddress+"tousgenesidentiques.gff","w")
+  f.writelines(newtab)
+  f.close()
 	
 def writeTSS_dat(directoryAddress):
-	global Range,Genome_pos,Genome_sign,Genome_id
-	newtab = []
-	newtab.append("TUindex\tTUorient\tTSS_pos\tTSS_strength\n")
-	for i in range(len(Genome_pos)):
-		newtab.append(str(i)+"\t"+Genome_sign[i]+"\t"+str(min(Genome_pos[i]))+"\t.2\n") # Note can't change TSS_strengh with this command yet !
-	f = open(directoryAddress+"TSS.dat","w")
-	f.writelines(newtab)
-	f.close()
+  global Range,Genome_pos,Genome_sign,Genome_id
+  newtab = []
+  newtab.append("TUindex\tTUorient\tTSS_pos\tTSS_strength\n")
+  for i in range(len(Genome_pos)):
+    newtab.append(str(i)+"\t"+Genome_sign[i]+"\t"+str(min(Genome_pos[i]))+"\t.2\n") # Note can't change TSS_strengh with this command yet !
+  f = open(directoryAddress+"TSS.dat","w")
+  f.writelines(newtab)
+  f.close()
 	
 def writeTTS_dat(directoryAddress):
-	global Range,Genome_pos,Genome_sign,Genome_id
-	newtab = []
-	newtab.append("TUindex\tTUorient\tTTS_pos\tTTS_proba_off\n")
-	for i in range(len(Genome_pos)):
-		newtab.append(str(i)+"\t"+Genome_sign[i]+"\t"+str(max(Genome_pos[i]))+"\t1.\n") # Note can't change TTS_proba_off with this command yet !
-	f = open(directoryAddress+"TTS.dat","w")
-	f.writelines(newtab)
-	f.close()
+  global Range,Genome_pos,Genome_sign,Genome_id
+  newtab = []
+  newtab.append("TUindex\tTUorient\tTTS_pos\tTTS_proba_off\n")
+  for i in range(len(Genome_pos)):
+    newtab.append(str(i)+"\t"+Genome_sign[i]+"\t"+str(max(Genome_pos[i]))+"\t1.\n") # Note can't change TTS_proba_off with this command yet !
+  f = open(directoryAddress+"TTS.dat","w")
+  f.writelines(newtab)
+  f.close()
 	
 
-
-
-def Metropolis (params){
+def Metropolis(params):
   
-  # il faut une grosse taille simulation pour estimer la fitness => peut dépendre architecture du génome
-  # simulation avec que des invertions => génome constant au début 
-  # poid relatif d'insertion délétion plutot que 
+  # il faut une grosse taille simulation pour estimer la fitness => peut dependre architecture du genome
+  # simulation avec que des invertions => genome constant au debut 
+  # poid relatif d'insertion deletion plutot que 
   #~ A = 0 # Note that this line is useless
   fitness = 0.5
   
   zyup()
   
-}
+
 
 def calc_fitness(Transcript) :
   goal_profile = [25,250,400,22,300,44,500,230,145,957]
@@ -130,7 +128,7 @@ def calc_fitness(Transcript) :
   return fitness
   
 
-def pic_interg_pos(nb_of_pos){
+def pic_interg_pos(nb_of_pos):
   pos_chosen = []
   # creating intergenic positions list
   interg_pos = []
@@ -147,7 +145,7 @@ def pic_interg_pos(nb_of_pos){
   for i in range(nb_of_pos) :
     pos_chosen.append(random.choice(interg_pos))
   return pos_chosen
-}
+
 
 def zyup(Genome_pos_new, Genome_sign_new) : 
   (pos_1,pos_2) = pic_interg_pos(2)
