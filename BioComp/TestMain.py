9bdcef1ccb328f5c0028f84a09894db364ca0b51
Acathignol <annie.cathignol@insa-lyon.fr>
2017-12-15 10:06:59 +0100

    faut genome pas en global _ part1

--- BioComp/TestMain.py
+++ BioComp/TestMain.py
@@ -54,6 +54,19 @@ def changeGenomeDir():
 	dostuff = 0
 
 
+
+def Metropolis (params){
+  
+  # il faut une grosse taille simulation pour estimer la fitness => peut dépendre architecture du génome
+  # simulation avec que des invertions => génome constant au début 
+  # poid relatif d'insertion délétion plutot que 
+  #~ A = 0 # Note that this line is useless
+  fitness = 0.5
+  
+  zyup()
+  
+}
+
 def pic_interg_pos(nb_of_pos){
   pos_chosen = []
   # creating intergenic positions list
@@ -73,38 +86,25 @@ def pic_interg_pos(nb_of_pos){
   return pos_chosen
 }
 
-#~ def Metropolis (params)
-  
-  #~ A = 0 # Note that this line is useless
-  
-def calc_fitness() :
-  goal_profile = [25,250,400,22,300,44,500,230,145,957]
-  fitness = 0
-  for i in xrange(len(Transcript)) : 
-    fitness = fitness + (goal_profile[i] - Transcript[i])*(goal_profile[i] - Transcript[i])
-  return fitness
-  
-
-
-def zyup() : 
+def zyup(Genome_pos_new, Genome_sign_new) : 
   (pos_1,pos_2) = pic_interg_pos(2)
-  for i,a in enumerate(Genome_pos) :
-    if (Genome_pos[i][0] >= pos_1 and Genome_pos[i][0] <= pos_2 and Genome_pos[i][1] >= pos_1 and Genome_pos[i][1] <= pos_2 ) :
-      if (Genome_sign[i] == "+") :
-        Genome_pos[i] = (pos_2 - (min(Genome_pos[i]) - pos_1), pos_1 + pos_2 - max(Genome_pos[i]))
-      	Genome_sign[i] = "-"
+  for i,a in enumerate(Genome_pos_new) :
+    if (Genome_pos_new[i][0] >= pos_1 and Genome_pos_new[i][0] <= pos_2 and Genome_pos_new[i][1] >= pos_1 and Genome_pos_new[i][1] <= pos_2 ) :
+      if (Genome_sign_new[i] == "+") :
+        Genome_pos_new[i] = (pos_2 - (min(Genome_pos_new[i]) - pos_1), pos_1 + pos_2 - max(Genome_pos_new[i]))
+      	Genome_sign_new[i] = "-"
       else : 
-        Genome_pos[i] = ( pos_1 + pos_2 - max(Genome_pos[i]),pos_2 - (min(Genome_pos[i]) - pos_1))
-	Genome_sign[i] = "+"
+        Genome_pos_new[i] = ( pos_1 + pos_2 - max(Genome_pos_new[i]),pos_2 - (min(Genome_pos_new[i]) - pos_1))
+	Genome_sign_new[i] = "+"
 
 
-def zyop() : 
+def zyop(Genome_pos_new) : 
   pos = pic_interg_pos(1)
   sign = random.choice(("+", "-"))
-  for i,a in enumerate(Genome_pos) :
-    if (min(Genome_pos[i]) > pos) :
+  for i,a in enumerate(Genome_pos_new) :
+    if (min(Genome_pos_new[i]) > pos) :
       if (sign == "+") :
-        Genome_pos[i] = (Genome_pos[i][0]+1, Genome_pos[i][1]+1)
+        Genome_pos_new[i] = (Genome_pos_new[i][0]+1, Genome_pos_new[i][1]+1)
       else : 
-        Genome_pos[i] = (Genome_pos[i][0]-1, Genome_pos[i][1]-1)
+        Genome_pos_new[i] = (Genome_pos_new[i][0]-1, Genome_pos_new[i][1]-1)
   
