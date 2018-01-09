df7d7a379ce3f4f4c86506db43573972ea599572
WaitndSee <antoine.weber@insa-lyon.fr>
2018-01-09 12:13:36 +0100

    Update main.py

--- BioComp/main.py
+++ BioComp/main.py
@@ -7,25 +7,22 @@ import random
 import sys 
 import TestMain
 
+
 # main script 
 
 print("Hello World!")
 
-nb_iterations = 1 # sys.argv[1]
+nb_iterations = 20000 # sys.argv[1]
 
 global goal_profile
 goal_profile = [25,250,400,22,300,44,500,230,145,957] # sys.argv[2] 
 
 global percent_zyup
-percent_zyup = 1 # sys.argv[3] # 1 = 100% = only inversion
+percent_zyup = 1 # sys.argv[3] # 1 = 100% = only indel
 
 global Genome_fitness
 Genome_fitness = 0
 # TestMain.calc_fitness(Transcrit) #problem : il est en global dans l'autre, antoine ? help !
 
 TestMain.Metropolis(nb_iterations)
-
-print(Genome_pos)
-
-
 
