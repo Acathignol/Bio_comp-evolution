505e1e38fad965b347072f41d892ce8da58f5baa
WaitndSee <antoine.weber@insa-lyon.fr>
2018-01-11 15:07:39 +0100

    Update showResults.R

--- BioComp/showResults.R
+++ BioComp/showResults.R
@@ -9,17 +9,23 @@ rawdata = read.table(file="SimulationResults.txt",header=F,sep="|")
 
 
 plot(rawdata$V2,main="fitness",xlab="fitness",type='l',lwd=2,col="darkorange1")
-points(rawdata$V3,main="fitness",xlab="fitness",col="royalblue2",add=T,pch=15)
+# points(rawdata$V3,main="fitness",xlab="fitness",col="royalblue2",add=T,pch=15)
 
-proba = (exp(rawdata$V3)-rawdata$V2)/(exp(1)-rawdata$V2)
+origin = c(250,250,250,250,250,250,250,250,250,250)
+target=c(25,250,400,22,300,44,500,230,145,957)
+which(rawdata$V2==max(rawdata$V2))[1]
 
-r = rawdata$V3/rawdata$V2
+counts <- data.frame("Origin"=origin,"Actual"=as.numeric(rawdata[which(rawdata$V2==max(rawdata$V2))[1],5:14]),"Target"= target)
+counts 
 
-proba = (exp(exp(exp(r)))-1)/(exp(exp(exp(1)))-1)
+barplot(as.matrix(counts))
 
-plot(proba,ylim=c(0,1),pch=15)
-plot(proba)
-plot(rapport)
 
-plot(proba,r)
+# r = rawdata$V3/rawdata$V2
+# 
+# proba = (exp(exp(exp(r)))-1)/(exp(exp(exp(1)))-1)
+# 
+# plot(proba,ylim=c(0,1),pch=15)
+# 
+# plot(proba,r)
 
