a2af19347a9e5ad2c12c79510a05f1cc1d44ffd3
DanielaCortes <daniela.cortes@insa-lyon.fr>
2018-01-11 15:27:19 +0100

    Update main.py

--- BioComp/main.py
+++ BioComp/main.py
@@ -17,12 +17,13 @@ nb_it_fitness = 10
 
 goal_profile =  [200, 250, 300, 300, 250, 200, 250, 300, 200, 250] # sys.argv[2] 
 
-global percent_zyup
-percent_zyup = 1 # sys.argv[3] # 1 = 100% = only indel
+#global percent_zyup
+percent_zyup = 0.5 # sys.argv[3] # 1 = 100% = only indel
+# 1, 0.75, 0.5 (et 0)
 
 #~ global Genome_fitness
 #~ Genome_fitness = 0
 # TestMain.calc_fitness(Transcrit) #problem : il est en global dans l'autre, antoine ? help !
 
-TestMain.Metropolis(nb_iterations, nb_it_fitness, goal_profile)
+TestMain.Metropolis(nb_iterations, nb_it_fitness, goal_profile, percent_zyup)
 
